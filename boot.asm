92d80d717ba4606bae5c05e0efd04e893480928f
Matthiasclee <matthias@matthiasclee.com>
2022-06-11 05:44:36 -0700

    Misc

--- boot.asm
+++ boot.asm
@@ -1,20 +1,59 @@
 [org 0x7c00]
 
-jmp codestart
-
-db "Matthias Lee"
+jmp main
 
 string1:
-  db "Bootloader started", 0x0d, 0x0a, 0
-string2:
-  db "Configuring registers", 0x0d, 0x0a, 0
-string3:
-  db "Additional sectors loaded successfully", 0
+  db "ML", 0
+prompt:
+  db 0x0d, 0x0a, "> ", 0
+bufflen:
+  db 0
+zero:
+  db 0
+cmdbuffer:
+  times 10 db 0
 
-codestart:
+main:
+  mov ax, 0
+  int 0x10
   mov bx, string1
   call print
-  jmp setup
+
+mainloop:
+  mov bx, prompt
+  call print
+  mov bx, bufflen
+  mov ah, [zero]
+  mov [bx], ah
+
+readloop:
+  call readchar
+  mov bh, [char]
+  mov ah, 32
+  mov al, 127
+  call checkifcontains
+  cmp ah, 1
+  je charpressed
+  mov bh, [scancode]
+  cmp bh, 28 
+  je mainloop
+  jmp readloop
+
+charpressed:
+  mov ah, [bufflen]
+  cmp ah, 10
+  je readloop
+  inc ah
+  mov bx, bufflen
+  mov [bx], ah
+  mov bx, cmdbuffer
+  add bx, [bufflen]
+  mov al, [char]
+  mov [bx], al
+  mov al, [bx]
+  mov ah, 0x0e
+  int 0x10
+  jmp readloop
 
 char:
   db 0
@@ -23,45 +62,53 @@ scancode:
   db 0
 
 readchar:
+  pusha
   mov ah, 0
   int 0x16
   mov bx, char
   mov [bx], al
   mov bx, scancode
   mov [bx], ah
+  popa
   ret
 
+checkifcontains:
+  pusha
+checkifcontains1:
+  cmp bh, ah
+  je checkifcontainssuccess
+  cmp ah, al
+  je checkifcontainsfail
+  inc ah
+  jmp checkifcontains1 
+checkifcontainssuccess:
+  popa
+  mov ah, 1
+  ret
+checkifcontainsfail:
+  popa
+  mov ah, 0
+  ret
+  
+
 print:
+  pusha
+print1:
   mov ah, 0x0e
   mov al, [bx]
   cmp al, 0
-  je return
+  je exitprint
+  push bx
+  mov bx, 0
   int 0x10
+  pop bx
   inc bx
-  jmp print
-
-return:
+  jmp print1
+exitprint:
+  popa
   ret
 
-setup:
-  mov bx, string2
-  call print
-
-  mov ax, 0
-  mov es, ax
-  mov ah, 2
-  mov al, 1
-  mov ch, 0
-  mov cl, 2
-  mov dh, 0
-  mov bx, 0x7e00
-
-continue:
-  int 0x13
-
-  mov bx, string3
-  call print
-  
+end:
   jmp $
   times 510-($-$$) db 0
   db 0x55, 0xaa
