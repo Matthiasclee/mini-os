29ec02dec46482b5ec13e895da0de17fd025ff55
Matthias Lee <matthias@matthiasclee.com>
2023-01-01 11:19:00 -0800

    Removed extra zero bytes

--- addons/mlfs/addon_os.asm
+++ addons/mlfs/addon_os.asm
@@ -2,9 +2,9 @@ jmp after_mlfs_loaded
 %include "addons/mlfs/setup_mlfs_structure.asm"
 %include "addons/mlfs/new_file.asm"
 flength:
-db 0x00, 0x00
+db 0x00
 fname:
-db 0x00, 0x00, 0x00, 0x00
+db 0x00, 0x00, 0x00
 after_mlfs_loaded:
 
 ;call setup_mlfs_structure
