515b264402a0edf6da4bfadef5e170d17b910710
Matthias Lee <matthias@matthiasclee.com>
2023-01-02 16:50:29 -0800

    Bootloader used local names

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,19 +1,21 @@
-jmp main
+jmp bootloader
 
-bootup_message:
-  db "ML bootloader", 0x0d, 0x0a, 0
-diskcheckfailedtext:
-  db "Error: disk check failed", 0
-diskcheckfailed:
-  mov bx, diskcheckfailedtext
-  call print
-  cli
-  hlt
+bootloader_messages:
+  .init_message:
+    db "ML bootloader", 0x0d, 0x0a, 0
+  .diskcheckfailed:
+    db "Error: disk check failed", 0
+bootloader_checks:
+  .diskcheckfailed:
+    mov bx, bootloader_messages.diskcheckfailed
+    call print
+    cli
+    hlt
 
 %include "lib/os_funcs.asm"
 
-main:
-  mov bx, bootup_message
+bootloader:
+  mov bx, bootloader_messages.init_message
   call print
 
   ; load more data
@@ -22,38 +24,39 @@ main:
   mov es, ax ;es can not be set directly
   mov bx, 0x7e00
   mov ah, 2
-  mov al, [loadsectors] ;sectors to load
+  mov al, [boot_opts.loadsectors] ;sectors to load
   mov ch, 0 ;cylinder number
   mov cl, 2 ;sector number
   mov dh, 0 ;head number
   int 0x13
 
   ; Check if data is valid
-  mov ah, [skipdatacheck]
+  mov ah, [boot_opts.skipdatacheck]
   cmp ah, 1
   je beginnext
   mov ah, [check]
   mov al, [check + 1]
   cmp ax, 0x4D09
   je beginnext
-  jne diskcheckfailed
+  jne bootloader_checks.diskcheckfailed
 
 endsector1:
   jmp $
   times 500-($-$$) db 0
   ; Boot flags
-  skipdatacheck:
+  boot_opts:
+    .skipdatacheck:
+      db 0x00
+    .loadsectors:
+      db 0x03
     db 0x00
-  loadsectors:
-    db 0x03
-  db 0x00
-  db 0x00
-  db 0x00
-  db 0x00
-  db 0x00
-  db 0x00
-  db 0x00
-  db 0x00
+    db 0x00
+    db 0x00
+    db 0x00
+    db 0x00
+    db 0x00
+    db 0x00
+    db 0x00
 
   ; Signature
   db 0x55, 0xaa
