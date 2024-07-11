1101f8719be4af16ca0942b691707adbecafe03f
Matthias Lee <matthias@matthiasclee.com>
2024-07-11 10:14:56 -0700

    Misc

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,23 +1,29 @@
+start:
+
 jmp bootloader
 
 %include "lib/std_functions/std_functions.asm"
 
 bootloader:
-  mov bx, 0x7e00
+  mov bx, .end
   mov al, [.loadsectors]
-  mov cl, 2
-  mov ch, 0
-  mov dh, 0
-  mov dl, 0x80
+  mov cl, 2 ; Sector #
+  mov ch, 0 ; Cyl #
+  mov dh, 0 ; Head #
+  mov dl, 0x80 ; Drive #
 
   call std.read_from_drive
+  jc .load_failed
 
+  .jump_to_entrypoint:
   jmp [.entrypoint]
 
   .loadsectors:
-  db 0x03
+    db 0x01 ; Number of sectors to load from disk
+  .ignore_load_failure:
+    db 0x00 ; Jump to entrypoint regardless of load failure
   .entrypoint:
-  dw entry_point
+    dw entry_point ; Point to jump to
   db 0x00
   db 0x00
   db 0x00
@@ -26,7 +32,18 @@ bootloader:
   db 0x00
   db 0x00
   db 0x00
-  db 0x00
+
+  .load_failed:
+    mov bx, .ignore_load_failure
+    mov ah, [bx]
+    cmp ah, 1
+    je .jump_to_entrypoint
+
+    mov bx, .load_failed_text
+    call std.print
+    cli
+    hlt
+  .load_failed_text: db "Failed to load sectors from disk.", 0
 
   times 446-($-$$) db 0
 
